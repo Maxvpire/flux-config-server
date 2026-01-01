ae24a9753df5f9193c60a8669bc1ef9a38d92a6f
Maxvpire <maxvpire@gmail.com>
2026-01-01 12:50:25 +0500

    all

---
+++ src/test/java/com/flux/config_service/FluxConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flux.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
